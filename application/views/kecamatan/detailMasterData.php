c6b31d9af50b6e44ef73f8aab5d8307e21de0073
unknown <rezzamuhammad12@gmail.com>
2020-09-24 20:12:52 +0700

    fixed detail kelompok tani

--- application/views/kecamatan/detailMasterData.php
+++ application/views/kecamatan/detailMasterData.php
@@ -1,14 +1,10 @@
-<?php
-var_dump($kelompokTani);
-?>
-
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- Page Heading -->
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
     <div class="row">
-        <table class="table table-striped table-detail-kelompok">
+        <table class="table table-striped table-kelompok-tani">
             <tbody>
                 <tr>
                     <td>Penyuluh Pendamping</td>
@@ -18,54 +14,54 @@ var_dump($kelompokTani);
                 <tr>
                     <td>Kota/Kabupaten</td>
                     <td>:</td>
-                    <td class="kota"><?= $kelompokTani[0]['kota']; ?></td>
+                    <td class="kota"><?= $kelompokTani[0]['kota_kab']; ?></td>
                 </tr>
                 <tr>
                     <td>Kecamatan</td>
                     <td>:</td>
-                    <td class="kecamatan"></td>
+                    <td class="kecamatan"><?= $kelompokTani[0]['kecamatan']; ?></td>
                 </tr>
                 <tr>
                     <td>desa</td>
                     <td>:</td>
-                    <td class="desa"></td>
+                    <td class="desa"><?= $kelompokTani[0]['desa']; ?></td>
                 </tr>
                 <tr>
                     <td>Alamat</td>
                     <td>:</td>
-                    <td class="alamat"></td>
+                    <td class="alamat"><?= $kelompokTani[0]['alamat']; ?></td>
                 </tr>
                 <tr>
                     <td>BPP</td>
                     <td>:</td>
-                    <td class="bpp"></td>
+                    <td class="bpp"><?= $kelompokTani[0]['bpp']; ?></td>
                 </tr>
                 <tr>
                     <td>Tahun Pembentukan</td>
                     <td>:</td>
-                    <td class="tahun_pembentukan"></td>
+                    <td class="tahun_pembentukan"><?= $kelompokTani[0]['tahun_pembentukan']; ?></td>
                 </tr>
                 <tr>
                     <td>Kelas</td>
                     <td>:</td>
-                    <td class="kelas"></td>
+                    <td class="kelas"><?= $kelompokTani[0]['kelas']; ?></td>
                 </tr>
                 <tr>
                     <td>Skor</td>
                     <td>:</td>
-                    <td class="skor"></td>
+                    <td class="skor"><?= $kelompokTani[0]['skor']; ?></td>
                 </tr>
 
                 <tr>
                     <td>Tahun Penerapan</td>
                     <td>:</td>
-                    <td class="tahun_penerapan"></td>
+                    <td class="tahun_penerapan"><?= $kelompokTani[0]['tahun_penerapan']; ?></td>
                 </tr>
 
                 <tr>
                     <td>Teknologi</td>
                     <td>:</td>
-                    <td class="teknologi"></td>
+                    <td class="teknologi"><?= $kelompokTani[0]['teknologi']; ?></td>
                 </tr>
             </tbody>
         </table>
